387656980ff9a452f4f5d2a0a7c27302c828fe07
Oukseh LEE <oukseh@codemind.co.kr>
2017-01-11 13:48:42 +0900

    first commit

---
+++ c/testcases/CWE78_OS_Command_Injection/fake_main.cpp
@@ -0,0 +1,24 @@
+/* NOTE - eventually this file will be automatically updated using a Perl script that understand
+ * the naming of test case files, functions, and namespaces.
+ */
+
+#include <time.h>   /* for time() */
+#include <stdlib.h> /* for srand() */
+
+#include "std_testcase.h"
+#include "testcases.h"
+
+int main(int argc, char * argv[]) {
+
+	/* seed randomness */
+
+	srand( (unsigned)time(NULL) );
+
+	globalArgc = argc;
+	globalArgv = argv;
+
+
+
+	return 0;
+
+} 
