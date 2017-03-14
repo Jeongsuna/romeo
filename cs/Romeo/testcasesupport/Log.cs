df10fc2f0c4c5406714ee2e0d36a813f2df7b77a
Jemin Kim (win) <jemin@codemind.co.kr>
2017-03-14 18:18:46 +0900

    Add c sharp in progress

---
+++ cs/Romeo/testcasesupport/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Romeo.testcasesupport
+{
+    class Log
+    {
+        public static void WriteLine(string msg)
+        {
+
+        }
+    }
+}
