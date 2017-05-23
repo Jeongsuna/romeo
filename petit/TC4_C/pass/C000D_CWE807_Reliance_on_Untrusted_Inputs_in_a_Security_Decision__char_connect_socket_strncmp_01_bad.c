c5920545c96f913befb71e5f2d6b32bbdb0b80d6
Jemin Kim (win) <jemin@codemind.co.kr>
2017-05-23 15:17:39 +0900

    TC4_C - Add C000D_CWE807

--- petit/TC4_C/pass/C000D_CWE807_Reliance_on_Untrusted_Inputs_in_a_Security_Decision__char_connect_socket_strncmp_01_bad.c
+++ petit/TC4_C/pass/C000D_CWE807_Reliance_on_Untrusted_Inputs_in_a_Security_Decision__char_connect_socket_strncmp_01_bad.c
@@ -1,15 +1,26 @@
 /*
-Filename : C000D_CWE807_Reliance_on_Untrusted_Inputs_in_a_Security_Decision__char_connect_socket_cookie_strncmp_01_bad.c
+Filename : C000D_CWE807_Reliance_on_Untrusted_Inputs_in_a_Security_Decision__char_connect_socket_strncmp_01_bad.c
 */
 
 #include "std_testcase.h"
 
-#include "cgic.h"
+#ifdef _WIN32
+# include <winsock2.h>
+# include <windows.h>
+# include <direct.h>
+# pragma comment(lib, "ws2_32") /* include ws2_32.lib when linking */
+# define CLOSE_SOCKET closesocket
+# define PATH_SZ 100
+#else /* NOT _WIN32 */
+# define INVALID_SOCKET -1
+# define SOCKET_ERROR -1
+# define CLOSE_SOCKET close
+# define SOCKET int
+# define PATH_SZ PATH_MAX
+#endif
 
 
 #include <wchar.h>
-
-
 
 #ifdef _WIN32
 # include <winsock2.h>
@@ -28,13 +39,9 @@ Filename : C000D_CWE807_Reliance_on_Untrusted_Inputs_in_a_Security_Decision__cha
 
 #define TCP_PORT 27015
 
-
-
-
-
 #ifndef OMITBAD
 
-void C000D_CWE807_Reliance_on_Untrusted_Inputs_in_a_Security_Decision__char_connect_socket_cookie_strncmp_01_bad()
+void C000D_CWE807_Reliance_on_Untrusted_Inputs_in_a_Security_Decision__char_connect_socket_strncmp_01_bad()
 {
     char * data;
     char data_buf[100] = "";
@@ -82,53 +89,18 @@ void C000D_CWE807_Reliance_on_Untrusted_Inputs_in_a_Security_Decision__char_conn
     }
 
     {
-        char * data = "trustedvalue";
-
-        char   **array, **arrayStep;
-        char   cname[1024], cvalue[1024];
-        fprintf(cgiOut, "Cookies Submitted On This Call, With Values "
-                "(Many Browsers NEVER Submit Cookies):<p>\n");
-        if (cgiCookies(&array) != cgiFormSuccess) {
-            return;
-        }
-        arrayStep = array;
-        fprintf(cgiOut, "<table border=1>\n");
-        fprintf(cgiOut, "<tr><th>Cookie<th>Value</tr>\n");
-        while (*arrayStep) {
-            char value[1024];
-            fprintf(cgiOut, "<tr>");
-            fprintf(cgiOut, "<td>");
-            cgiHtmlEscape(*arrayStep);
-            fprintf(cgiOut, "<td>");
-            cgiCookieString(*arrayStep, value, sizeof(value));
-            cgiHtmlEscape(value);
-            fprintf(cgiOut, "\n");
-            arrayStep++;
-        }
-        fprintf(cgiOut, "</table>\n");
-        cgiFormString("cname", cname, sizeof(cname));  
-        cgiFormString("cvalue", cvalue, sizeof(cvalue));
+        char trusted = 0;
+        struct hostent *hp;
+        struct in_addr myaddr;
+        myaddr.s_addr = inet_addr("127.0.0.1");
         
+        hp = gethostbyaddr((char *) &myaddr, sizeof(struct in_addr), AF_INET);
         /* FLAW */
-        if (strlen(cname) && !strncmp(cvalue, data, sizeof(data)))
-        {
-            fprintf(cgiOut, "New Cookie Set On This Call:<p>\n");
-            fprintf(cgiOut, "Name: ");  
-            cgiHtmlEscape(cname);
-            fprintf(cgiOut, "Value: ");  
-            cgiHtmlEscape(cvalue);
-            fprintf(cgiOut, "<p>\n");
-            fprintf(cgiOut, "If your browser accepts cookies "
-                    "(many do not), this new cookie should appear "
-                    "in the above list the next time the form is "
-                    "submitted.<p>\n"); 
+        if (hp && !strncmp(hp->h_name, data, sizeof(data))) {
+            trusted = 1;
+        } else {
+            trusted = 0;
         }
-        else
-        {
-            fprintf(cgiOut, "Untrusted values<p>\n");        
-        }
-    
-        cgiStringArrayFree(array);
     }
 
 }
@@ -149,7 +121,7 @@ int main(int argc, char * argv[])
     srand( (unsigned)time(NULL) );
 #ifndef OMITBAD
     printLine("Calling bad()...");
-    C000D_CWE807_Reliance_on_Untrusted_Inputs_in_a_Security_Decision__char_connect_socket_cookie_strncmp_01_bad();
+    C000D_CWE807_Reliance_on_Untrusted_Inputs_in_a_Security_Decision__char_connect_socket_strncmp_01_bad();
     printLine("Finished bad()");
 #endif /* OMITBAD */
     return 0;
