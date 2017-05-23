9f74038df278ff4b204b43fb4686a79147772de6
Oukseh LEE <oukseh@codemind.co.kr>
2017-05-23 19:36:08 +0900

    C 0035,0036

---
+++ petit/TC4_C/pass/C0036_CERT_INT34_C__Shift_more_bit_than_width.c
@@ -0,0 +1,15 @@
+#include "std_testcase.h"
+
+#ifndef OMITBAD
+
+int bad()
+{
+    int data;
+    /* POTENTIAL FLAW: Set data to zero */
+    data = 33;
+    /* POTENTIAL FLAW: Possibly divide by zero */
+    printIntLine(32 << data);
+}
+
+#endif /* OMITBAD */
+
