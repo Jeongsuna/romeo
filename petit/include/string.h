92a17e5979aa6c914bd2e94e2312a7507e518efe
rho <rho@codemind.co.kr>
2017-05-23 14:42:37 +0900

    includes fixed

--- petit/include/string.h
+++ petit/include/string.h
@@ -6,3 +6,4 @@ void* memcpy(void* __dest, const void* __src, size_t __n);
 void* memset(void* __s, int __c, size_t __n);
 
 inline char* strchr(char* const _String, int const _Ch);
+char * strstr (char * str1, const char * str2 );
