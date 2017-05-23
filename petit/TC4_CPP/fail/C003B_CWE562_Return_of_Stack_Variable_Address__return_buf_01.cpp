21605e633174552139ed56bda31cb6d5a220addc
Oukseh LEE <oukseh@codemind.co.kr>
2017-05-23 19:20:28 +0900

    Cxx C003B

--- petit/TC4_CPP/fail/C003B_CWE562_Return_of_Stack_Variable_Address__return_buf_01.cpp
+++ petit/TC4_CPP/fail/C003B_CWE562_Return_of_Stack_Variable_Address__return_buf_01.cpp
@@ -1,5 +1,5 @@
 /*
- * @description Return of local buffer address via stack-allocated object
+ * @description Return of local buffer address to local buffer
  * 
  * */
 
@@ -7,40 +7,22 @@
 
 /* this REQUIRES secondary functions */
 
-namespace CWE562_Return_of_Stack_Variable_Address__return_local_class_member_01 
-{
-    
-class HelperClass 
-{
-    private:
-        int intArray[2];
-    
-    public:
-        HelperClass() 
-        { 
-            intArray[0] = 0x41; 
-            intArray[1] = 0x42; 
-        }
-        
-        const int *getIntArray() 
-        { 
-            return intArray; 
-        }
-};
-
 #ifndef OMITBAD
 
-static const int *helperBad()
+class A { public: int x; };
+
+static A *helperBad()
 {
-    HelperClass HelperClass;
+    A ret;
+    ret.x = 0;
 
     /* FLAW: returning stack-allocated buffer */
-    return HelperClass.getIntArray(); /* this may generate a warning -- it's on purpose */
+    return &ret; /* this may generate a warning -- it's on purpose */
 }
 
-void bad() 
+void CWE562_Return_of_Stack_Variable_Address__return_buf_01_bad() 
 {
-    printIntLine(*helperBad());
+    printLine(helperBad()->x);
 }
 
 #endif /* OMITBAD */
@@ -48,31 +30,29 @@ void bad()
 
 #ifndef OMITGOOD
 
-static const int *helperGood1()
+static char *helperGood1()
 {
-    static HelperClass HelperClass;
+    static char charString[] = "helperGood1 string";
 
     /* FIX: don't return a stack-allocated buffer
      * you can use static (i.e., global) variables but this renders your
      * code, and all code that uses it, non-re-entrant and non-threadsafe,
      * and hence is not a complete solution.  We do it anyway
      */
-    return HelperClass.getIntArray();
+    return charString;
 }
 
 static void good1() 
 {
-    printIntLine(*helperGood1());
+    printLine(helperGood1());
 }
 
-void good() 
+void CWE562_Return_of_Stack_Variable_Address__return_buf_01_good() 
 {
     good1();
 }
 
 #endif /* OMITGOOD */
-
-};
 
 /* Below is the main(). It is only used when building this testcase on 
  * its own for testing or for building a binary to use in testing binary 
@@ -82,8 +62,6 @@ void good()
 
 #ifdef INCLUDEMAIN
 
-using namespace CWE562_Return_of_Stack_Variable_Address__return_local_class_member_01; /* so that we can use good and bad easily */
-
 int main(int argc, char * argv[]) 
 {
     /* seed randomness */
@@ -91,13 +69,13 @@ int main(int argc, char * argv[])
 
 #ifndef OMITGOOD
     printLine("Calling good()...");
-    good();
+    CWE562_Return_of_Stack_Variable_Address__return_buf_01_good();
     printLine("Finished good()");
 #endif /* OMITGOOD */
 
 #ifndef OMITBAD
     printLine("Calling bad()...");
-    bad();
+    CWE562_Return_of_Stack_Variable_Address__return_buf_01_bad();
     printLine("Finished bad()");
 #endif /* OMITBAD */
 
