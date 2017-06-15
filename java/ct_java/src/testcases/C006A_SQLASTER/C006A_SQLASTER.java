a4988bc9d360f9ecf888673766917ab03404583d
rho <rho@codemind.co.kr>
2017-06-15 11:45:27 +0900

    006A, 006B sample added

---
+++ java/ct_java/src/testcases/C006A_SQLASTER/C006A_SQLASTER.java
@@ -0,0 +1,48 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class C006A_SQLMATCH_bad 
+{
+    public void bad(Connection con) throws Throwable
+    {
+		String select = "SEleCT * ";
+		
+    	String where = "and address = ?";
+    	PreparedStatement p = con.prepareStatement(select + "from people where (first_name = ? or last_name = ?) " + where);
+    			p.setString(1, "a");
+    			p.setString(2, "b");
+    			p.setString(3, "c");
+    }
+	
+    public void good(Connection con) throws Throwable
+    {
+    	String where = "and address = ?";
+    	PreparedStatement p = con.prepareStatement("select first_name from people where (first_name = ? or last_name = ?) " + where);
+    			p.setString(1, "a");
+    			p.setString(2, "b");
+    			p.setString(3, "c");
+    }
+	
+    public void bad2(Connection con) throws Throwable
+    {
+    	String where = "and address = ?";
+    	PreparedStatement p = con.prepareStatement("SELECT * from people where (first_name = ? or last_name = ?) " + where);
+    			p.setString(1, "a");
+    			p.setString(2, "b");
+    			p.setString(3, "c");
+    }
+    public void bad3(Connection con) throws Throwable
+    {
+    	String where = "and (last_name LIKE '%')";
+    	PreparedStatement p = con.prepareStatement("SELECT first_name from people where (first_name = ? or last_name = ?) " + where);
+    			p.setString(1, "a");
+    			p.setString(2, "b");
+    }
+    public void bad4(Connection con) throws Throwable
+    {
+    	String where = "and (last_name LiKE \"%\")";
+    	PreparedStatement p = con.prepareStatement("SELECT last_name from people where (first_name = ? or last_name = ?) " + where);
+    			p.setString(1, "a");
+    			p.setString(2, "b");
+    }
+}
