f42489e6a27fba3f98cb71d268219b321ecb4bd8
Jemin Kim (win) <jemin@codemind.co.kr>
2017-09-26 18:59:56 +0900

    Java - Add LEAKSESS samples

---
+++ java/src/testcases/C0029_LEAKSESS_CWE488/C0029_LEAKSESS__Controller_01_bad.java
@@ -0,0 +1,11 @@
+package testcases.C0029_LEAKSESS_CWE488;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class C0029_LEAKSESS__Controller_01_bad {
+	private int currentPage = 1;
+	public void doSomething(HttpServletRequest request) {
+		currentPage = Integer.parseInt(request.getParameter("page"));
+	}
+}
