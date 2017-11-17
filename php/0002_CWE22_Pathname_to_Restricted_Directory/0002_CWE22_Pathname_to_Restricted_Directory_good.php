7c7860639847b77fb5ac0b20beda6cbc939d0589
hyobeom <hyobeom@codemind.co.kr>
2017-11-17 16:51:40 +0900

    PHP CWE22

---
+++ php/0002_CWE22_Pathname_to_Restricted_Directory/0002_CWE22_Pathname_to_Restricted_Directory_good.php
@@ -0,0 +1,18 @@
+<?php
+
+$dataPath = "/users/cwe/profiles";
+$username = $_GET["user"];
+$profilePath = $dataPath . "/" . $username.".txt";
+
+
+// check directory path using str_replace
+$checkedPath = str_replace("/","",profilePath);
+$checkedPath = str_replace("/","",checkedPath);
+
+$myfile = fopen($profilePath, "r") or die("Unable to open file : " . $profilePath);
+echo fread($myfile,filesize($profilePath));
+fclose($myfile);
+
+
+?>
+
