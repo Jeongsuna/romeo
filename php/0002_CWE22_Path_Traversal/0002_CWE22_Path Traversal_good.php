a12e4af51f36669519ae35247fd02c0b4b2b5046
hyobeom <hyobeom@codemind.co.kr>
2017-11-21 15:39:47 +0900

    PHP update

--- php/0002_CWE22_Path_Traversal/0002_CWE22_Path Traversal_good.php
+++ php/0002_CWE22_Path_Traversal/0002_CWE22_Path Traversal_good.php
@@ -2,11 +2,11 @@
 
 $dataPath = "/users/cwe/profiles";
 $username = $_GET["user"];
+// check directory path using str_replace
+$username = preg_replace("/[ #\&\+\-%@=\/\\\:;,\.'\"\^`~\_|\!\?\*$#<>()\[\]\{\}]/i", "", $username);
+
 $profilePath = $dataPath . "/" . $username.".txt";
 
-
-// check directory path using str_replace
-$profilePath = $string = preg_replace("/[ #\&\+\-%@=\/\\\:;,\.'\"\^`~\_|\!\?\*$#<>()\[\]\{\}]/i", "", $profilePath);
 
 
 $myfile = fopen($profilePath, "r") or die("Unable to open file : " . $profilePath);
