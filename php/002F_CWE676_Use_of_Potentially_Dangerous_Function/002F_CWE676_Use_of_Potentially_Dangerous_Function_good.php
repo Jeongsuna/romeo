48afc25134e876885e0a7db4e526ba130361a4ad
Jemin Kim <jemin@codemind.co.kr>
2017-11-24 18:17:24 +0900

    PHP - Add 002F DANGERAPI example

---
+++ php/002F_CWE676_Use_of_Potentially_Dangerous_Function/002F_CWE676_Use_of_Potentially_Dangerous_Function_good.php
@@ -0,0 +1,26 @@
+<?php
+
+ $target = "pictures /". basename ($_FILES [ 'uploadedfile'] [ 'name']));
+
+
+ $ext = array_pop(explode(".", strtolower($target)));
+
+// fix
+ if(@preg_match($ext, "jpg|png|gif|jpeg")){ 
+
+ 	if (move_uploaded_file ($_FILES [ 'uploadedfile'] [ 'tmp_name'], $target)) 
+	{
+	echo "The picture has been successfully uploaded.";
+
+	} 
+	else 
+	{	
+	echo "There was an error uploading the picture, please try again.";
+
+	}    
+} else{
+	echo "Invalid extension on file.";
+}
+
+
+?>
