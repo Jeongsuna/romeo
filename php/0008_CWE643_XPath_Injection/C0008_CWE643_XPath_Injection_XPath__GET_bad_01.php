bd791367f363eb8c43ae827dadaae52a545b1ea3
Jemin Kim <jemin@codemind.co.kr>
2017-12-04 18:24:28 +0900

    PHP - Remove XML Injection and Fix names

--- php/0008_CWE643_XPath_Injection/C0008_CWE643_XPath_Injection_XPath__GET_bad_01.php
+++ php/0008_CWE643_XPath_Injection/C0008_CWE643_XPath_Injection_XPath__GET_bad_01.php
@@ -52,7 +52,7 @@ $query = "//User[@username='". $tainted . "']";
 $xml = simplexml_load_file("users.xml");//file load
 echo "query : ". $query ."<br /><br />" ;
 
-// Flaw:
+// flaw
 $res=$xml->xpath($query);//execution
 print_r($res);
 echo "<br />" ;
--- php/0019_CWE321_Use_of_Hard_coded_Cryptographic_Key/C0019__CWE321_Use_of_Hard_coded_Cryptographic_Key_bad_01.php
+++ php/0019_CWE321_Use_of_Hard_coded_Cryptographic_Key/C0019__CWE321_Use_of_Hard_coded_Cryptographic_Key_bad_01.php
@@ -1,6 +1,6 @@
 <?php
 
-function VerifyAdmin ($password) {
+function VerifyAdminBad($password) {
 	//flaw
 	if (strcmp($password, "68af404b513073584c4b6f22b6c63e6b")==0) {
 		 return true;
