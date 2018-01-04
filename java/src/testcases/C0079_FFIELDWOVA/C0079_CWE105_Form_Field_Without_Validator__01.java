fe61c7444cecc9bb81bfb5b29d5542f107c5a6c2
juho <juho@codemind.co.kr>
2018-01-04 13:53:24 +0900

    [framework] add EJB, Struts sample

---
+++ java/src/testcases/C0079_FFIELDWOVA/C0079_CWE105_Form_Field_Without_Validator__01.java
@@ -0,0 +1,29 @@
+public class C0079_CWE105_Form_Field_Without_Validator__01 {
+	public class RegistrationVFormBad extends org.apache.struts.validator.ValidatorForm {
+		// private variables for registration form
+		private String name;
+		private String address;
+		private String city;
+		private String state;
+		private String zipcode;
+		private String phone;
+		private String email;
+
+		public RegistrationFormBad() {  super();  }
+		// getter and setter methods for private variables
+	}
+
+	public class RegistrationVFormGood extends org.apache.struts.validator.ValidatorForm {
+		// private variables for registration form
+		private String name;
+		private String address;
+		private String city;
+		private String state;
+		private String zipcode;
+		private String phone;
+		private String email;
+
+		public RegistrationFormGood() {  super();  }
+		// getter and setter methods for private variables
+	}
+}
