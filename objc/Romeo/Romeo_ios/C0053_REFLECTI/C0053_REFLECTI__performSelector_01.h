729fb8796352cb620aaace948b3907b0e01e3eb2
jeminkim <jeminya@naver.com>
2018-01-17 14:27:50 +0900

    ObjC - Add 0053 REFLECTI samples

---
+++ objc/Romeo/Romeo_ios/C0053_REFLECTI/C0053_REFLECTI__performSelector_01.h
@@ -0,0 +1,16 @@
+//
+//  C0053_REFLECTI__performSelector_01.h
+//  Romeo
+//
+//  Created by Jemin Kim on 2018. 1. 17..
+//  Copyright © 2018년 Jemin Kim. All rights reserved.
+//
+
+#ifndef C0053_REFLECTI__performSelector_01_h
+#define C0053_REFLECTI__performSelector_01_h
+
+@interface C0053_REFLECTI__performSelector_01 : NSObject
+-(void) bad: (NSString*) arg obj:(NSString*) obj;
+@end
+
+#endif /* C0053_REFLECTI__simple_01_h */
