09ba94fb1991bd676758e9aee95fb1b911ce82dc
HyoBeom <hyobeom@codemind.co.kr>
2018-01-17 14:25:57 +0900

    Objc C0016 추가

---
+++ objc/Romeo/Romeo_ios/C0016_HARDPASS/C0016_HARDPASS_k_password__bad.m
@@ -0,0 +1,23 @@
+//
+//  C0016_HARDPASS_bad.m
+//  Romeo
+//
+//  Created by HyoBeom on 2018. 1. 17..
+//  Copyright © 2018년 Jemin Kim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "C0016_HARDPASS.h"
+
+@implementation C0016_HARDPASS_bad : NSObject
+
+-(void) bad:(NSString *) example{
+  
+    //flaw : using Hard-coded Password
+    // id : 123121212
+    // 비밀번호 : 10dadf1!23
+    
+}
+
+@end
