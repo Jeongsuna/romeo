21a6bc8488305e796a3e569eb1869d5e89ac787a
HyoBeom <hyobeom@codemind.co.kr>
2018-01-18 20:59:10 +0900

    C0024_NOCHKERR add

---
+++ objc/Romeo/Romeo_ios/C0024_NOCHKERR/C0024_NOCHKERR.h
@@ -0,0 +1,19 @@
+//
+//  C0024_NOCHKERR__simple_01.h
+//  Romeo
+//
+//  Created by Jemin Kim on 2018. 1. 16..
+//  Copyright © 2018년 Jemin Kim. All rights reserved.
+//
+
+#ifndef C0024_NOCHKERR_h
+#define C0024_NOCHKERR_h
+
+@interface C0024_NOCHKERR_bad : NSObject
+    -(void ) bad:(NSArray *) arr;
+@end
+
+@interface C0024_NOCHKERR_good : NSObject
+-(void ) good:(NSArray *) arr;
+@end
+#endif /* C0024_NOCHKERR__simple_01_h */
---
+++ objc/Romeo/Romeo_ios/C0024_NOCHKERR/C0024_NOCHKERR__bad.m
@@ -0,0 +1,34 @@
+//
+//  C0024_NOCHKERR__simple_01.m
+//  Romeo
+//
+//  Created by Jemin Kim on 2018. 1. 16..
+//  Copyright © 2018년 Jemin Kim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "C0024_NOCHKERR.h"
+
+@implementation C0024_NOCHKERR_bad : NSObject
+
+    -(void) bad:(NSArray *) arr {
+    
+        @try {
+            
+            NSString *result = [NSString stringWithFormat: @"%@", [arr objectAtIndex:11]];
+        
+        }
+        @catch (NSException * e) {
+    
+            /* FLAW: unprecise Exception Handling */
+            NSString *result = @"defaultString";
+            NSLog(@"Error : NSRangeException");
+    
+        }
+        @finally {
+            NSLog(@"Finally executes no matter what");
+        }
+    }
+
+@end
+
