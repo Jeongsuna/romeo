8f5a7f7be165358ad6bdc79a9af47887c735713b
HyoBeom <hyobeom@codemind.co.kr>
2018-01-22 11:14:29 +0900

    Objc C0039_FREELOCAL

---
+++ objc/Romeo/Romeo_ios/C0039_FREELOCAL/m01/CWE590_Free_Memory_Not_on_Heap.h
@@ -0,0 +1,13 @@
+//
+//  CWE590_Free_Memory_Not_on_Heap.h
+//  Romeo
+//
+//  Created by HyoBeom on 2018. 1. 19..
+//  Copyright © 2018년 Jemin Kim. All rights reserved.
+//
+
+#ifndef CWE590_Free_Memory_Not_on_Heap_h
+#define CWE590_Free_Memory_Not_on_Heap_h
+
+
+#endif /* CWE590_Free_Memory_Not_on_Heap_h */
---
+++ objc/Romeo/Romeo_ios/C0039_FREELOCAL/objectiveC/C0039_FREELOCAL_CHAR_MIN__bad.m
@@ -0,0 +1,21 @@
+//
+//  C0016_HARDPASS_bad.m
+//  Romeo
+//
+//  Created by HyoBeom on 2018. 1. 17..
+//  Copyright © 2018년 Jemin Kim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "C0039_FREELOCAL.h"
+
+@implementation C0039_FREELOCAL_bad : NSObject
+
+-(void) bad:(NSString *) example{
+    
+    //flaw:
+    free(CHAR_MIN);
+}
+
+@end
