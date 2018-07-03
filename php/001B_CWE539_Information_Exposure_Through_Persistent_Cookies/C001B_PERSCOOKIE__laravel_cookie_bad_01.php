b7f2b9912fbc2d8a2da94b1b69b2d63443985633
Jemin Kim <jemin@codemind.co.kr>
2018-07-03 16:42:35 +0900

    PHP - Add 0001 SQLI, 001B PERCOOKIE, 005A SESEXPIRE samples

---
+++ php/001B_CWE539_Information_Exposure_Through_Persistent_Cookies/C001B_PERSCOOKIE__laravel_cookie_bad_01.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class C001B_PERSCOOKIE__laravel_cookie_bad_01 extends Controller {
+    public function setCookie(Request $request){
+        $response = new Response('Hello World');
+        // flaw:
+        $response->withCookie(cookie('name', 'virat', 11520));  // min (8 days)
+        return $response;
+    }
+    public function getCookie(Request $request){
+        $value = $request->cookie('name');
+        echo $value;
+    }
+}
