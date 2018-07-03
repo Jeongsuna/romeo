43df3a6d4d8f2ae4849147f090311a1631141ba7
Jemin Kim <jemin@codemind.co.kr>
2018-07-03 18:10:53 +0900

    PHP - 0006 REDIRECT sample

---
+++ php/0006_CWE601_Open_Redirect/C0006_REDIRECT__laravel_redirect_away_bad_01.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
+class C0006_REDIRECT__laravel_redirect_away_bad_01 extends Controller {
+    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  Illuminate\Http\Request  $request
+     * @return Response
+     */
+    public function bad(Request $request)
+    {
+        $site = $request->input('site');
+        // flaw
+        return redirect()->away($site);
+    }
+}
