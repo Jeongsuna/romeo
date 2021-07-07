d173bba9ed3250309227ac7be32f5c269518481c
jemin <jemin@codemind.co.kr>
2021-07-07 15:28:41 +0900

    C - Add more 000C TYPEOVER samples

--- c/C000C_TYPEOVER__CWE190_Integer_Overflow/s01/C000C_TYPEOVER__ftell_simple_01.c
+++ c/C000C_TYPEOVER__CWE190_Integer_Overflow/s01/C000C_TYPEOVER__ftell_simple_01.c
@@ -2,16 +2,28 @@
 
 void C000C_TYPEOVER__ftell_simple_01_bad()
 {
-	short nSize;
+	int nSize;
 	FILE *fp = fopen("my_file.txt", "rb");
 	if(fp) {
+		fseek(fp, 0, SEEK_END);
+		// flaw: nSize - int, ftell ret - long
+		nSize = ftell(fp);
+		fclose(fp);
+	}
+}
+/*
+void C000C_TYPEOVER__ftell_simple_01_bad2()
+{
+	short nSize;
+	FILE* fp = fopen("my_file.txt", "rb");
+	if (fp) {
 		fseek(fp, 0, SEEK_END);
 		// flaw: nSize - short, ftell ret - long
 		nSize = ftell(fp);
 		fclose(fp);
 	}
 }
-
+*/
 void C000C_TYPEOVER__ftell_simple_01_good()
 {
 	long nSize;
