4309d42cfe51a83852cc38f59e96055a166f9994
Kim KyengJun <kkj@codemind.co.kr>
2022-05-11 15:41:08 +0900

    [KILD] romeo C901B_USEDATE 예제 소스코드 수정
    추가적인 케이스 관련 예제 내용 추가

--- klid/C901B_USEDATE/USEDATE_BAD.java
+++ klid/C901B_USEDATE/USEDATE_BAD.java
@@ -5,8 +5,14 @@ import java.util.GregorianCalendar;
 
 public class USEDATE_BAD {
     public static void main(String[] args) {
-        Calendar c = new GregorianCalendar(2022, 12, 25);
-        if (c.get(Calendar.MONTH) == 12) {
+
+        Date d = new Date();
+        d.setDate(25);
+        d.setYear(2014);
+        d.setMonth(12);  // Noncompliant; rolls d into the next year
+
+        Calendar c = new GregorianCalendar(2022, 12, 25); // Noncompliant
+        if (c.get(Calendar.MONTH) == 12) {  // Noncompliant; invalid comparison
             // ...
         }
     }
--- klid/C901B_USEDATE/USEDATE_GOOD.java
+++ klid/C901B_USEDATE/USEDATE_GOOD.java
@@ -5,6 +5,12 @@ import java.util.GregorianCalendar;
 
 public class USEDATE_GOOD {
     public static void main(String[] args) {
+        
+        Date d = new Date();
+        d.setDate(25);
+        d.setYear(2014);
+        d.setMonth(11);
+
         Calendar c = new GregorianCalendar(2022, 11, 25);
         if (c.get(Calendar.MONTH) == 11) {
             // ...
