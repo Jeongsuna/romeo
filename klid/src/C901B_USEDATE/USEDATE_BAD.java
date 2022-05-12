7a815f57a1c067c6ceb8eca29b12684b3902baf6
Kim KyengJun <kkj@codemind.co.kr>
2022-05-12 17:47:29 +0900

    [KILD] C901B 테스트 케이스 추가

--- klid/src/C901B_USEDATE/USEDATE_BAD.java
+++ klid/src/C901B_USEDATE/USEDATE_BAD.java
@@ -11,9 +11,26 @@ public class USEDATE_BAD {
         d.setDate(25);
         d.setYear(2014);
         d.setMonth(12);  // Noncompliant; rolls d into the next year
+        d.setDate(32);  // Noncompliant; rolls d into the next month
+        d.setHours(24);  // Noncompliant; rolls d into the next date
+        d.setMinutes(60);  // Noncompliant; rolls d into the next hour
+        d.setSeconds(60);  // Noncompliant; rolls d into the next minute
 
-        Calendar c = new GregorianCalendar(2022, 12, 25); // Noncompliant
-        if (c.get(Calendar.MONTH) == 12) {  // Noncompliant; invalid comparison
+        Calendar c = new GregorianCalendar(2022, 12, 25, 24, 59, 59); // Noncompliant
+
+        if (c.get(Calendar.MONTH) == 12 && (24 == c.get(Calendar.HOUR_OF_DAY)) && (12 == c.get(Calendar.HOUR))) {  // Noncompliant; invalid comparison
+            // ...
+        }
+
+        if (12 == c.get(Calendar.HOUR)) {  // Noncompliant; invalid comparison
+            // ...
+        }
+
+        if (c.get(Calendar.HOUR) == 12) {  // Noncompliant; invalid comparison
+            // ...
+        }
+
+        if (12 == c.get(Calendar.MONTH)) {  // Noncompliant; invalid comparison
             // ...
         }
     }
