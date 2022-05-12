e054e29d36481d1b5a2d6c7f2e45eaeb2d87f518
jemin <jemin@codemind.co.kr>
2022-05-12 13:10:06 +0900

    enhance klid 9006 samples

--- klid/src/C9006_WRAPCLASSINSTANT/WRAPCLASSINSTANT_BAD.java
+++ klid/src/C9006_WRAPCLASSINSTANT/WRAPCLASSINSTANT_BAD.java
@@ -1,8 +1,17 @@
 package C9006_WRAPCLASSINSTANT;
 
 public class WRAPCLASSINSTANT_BAD {
-    public static void main(String[] args) {
-        int myInteger = 0;
+    public static void bad(int myInteger, boolean myBoolean, byte myByte, char myCharacter, double myDouble,
+                           float myFloat, long myLong, short myShort, String myString) {
         new Integer(myInteger).toString();      // Noncompliant
+        new Boolean(myBoolean).toString();      // Noncompliant
+        new Byte(myByte).toString();            // Noncompliant
+        new Character(myCharacter).toString();  // Noncompliant
+        new Double(myDouble).toString();        // Noncompliant
+        new Float(myFloat).toString();          // Noncompliant
+        new Long(myLong).toString();            // Noncompliant
+        new Short(myShort).toString();          // Noncompliant
+        new String(myString).toString();        // Noncompliant
     }
 }
+
--- klid/src/C9006_WRAPCLASSINSTANT/WRAPCLASSINSTANT_GOOD.java
+++ klid/src/C9006_WRAPCLASSINSTANT/WRAPCLASSINSTANT_GOOD.java
@@ -1,8 +1,15 @@
 package C9006_WRAPCLASSINSTANT;
 
 public class WRAPCLASSINSTANT_GOOD {
-    public static void main(String[] args) {
-        int myInteger = 0;
-        Integer.toString(myInteger);        // Compliant
+    public static void good(int myInteger, boolean myBoolean, byte myByte, char myCharacter, double myDouble,
+                            float myFloat, long myLong, short myShort, String myString) {
+        Integer.toString(myInteger);      // Compliant
+        Boolean.toString(myBoolean);      // Compliant
+        Byte.toString(myByte);            // Compliant
+        Character.toString(myCharacter);  // Compliant
+        Double.toString(myDouble);        // Compliant
+        Float.toString(myFloat);          // Compliant
+        Long.toString(myLong);            // Compliant
+        Short.toString(myShort);          // Compliant
     }
 }
