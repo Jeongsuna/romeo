7d9d01a2e156f6dd4bf1cf8c33c072de2bdcfe5c
Kim KyengJun <kkj@codemind.co.kr>
2022-05-16 17:52:15 +0900

    [KILD] 테스트 소스코드 수정
    C903A 옳바른 소스코드 내용 정정

--- klid/src/C903A_UPDATESTATICFIELD/UPDATESTATICFIELD_Good.java
+++ klid/src/C903A_UPDATESTATICFIELD/UPDATESTATICFIELD_Good.java
@@ -3,7 +3,7 @@ package C903A_UPDATESTATICFIELD;
 import java.util.Date;
 
 public class UPDATESTATICFIELD_Good {
-    static Date birth;
+    Date birth;
     static int ex = 10;
 
     public UPDATESTATICFIELD_Good(Date birthday) {
