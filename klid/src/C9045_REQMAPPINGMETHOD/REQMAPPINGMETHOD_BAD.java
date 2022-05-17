96f4722df0ed5b1cfdb94aba8e05c4d6e88e71b0
Kim KyengJun <kkj@codemind.co.kr>
2022-05-17 17:12:22 +0900

    [KILD] C9045 테스트 코드 수정
    하나의 케이스가 아닌 복수개의 어노테이션에 관한 테스트 코드 추가

--- klid/src/C9045_REQMAPPINGMETHOD/REQMAPPINGMETHOD_BAD.java
+++ klid/src/C9045_REQMAPPINGMETHOD/REQMAPPINGMETHOD_BAD.java
@@ -1,14 +1,45 @@
 package C9045_REQMAPPINGMETHOD;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+@Controller
 public class REQMAPPINGMETHOD_BAD {
 
   @RequestMapping(value = "/greet", method = GET)
   private String greet(String greetee) {
-    // do something
-    return null;
+      // do something
+      return null;
   }
-  
+
+  @GetMapping("/greet")
+  private String greet2(String greetee) {
+      // do something
+      return null;
+  }
+
+  @PutMapping("/greet")
+  private String greet3(String greetee) {
+      // do something
+      return null;
+  }
+
+  @DeleteMapping("/greet")
+  private String greet4(String greetee) {
+      // do something
+      return null;
+  }
+
+  @PatchMapping("/greet")
+  private String greet5(String greetee) {
+      // do something
+      return null;
+  }
+
+  @PostMapping("/greet")
+  private String greet6(String greetee) {
+      // do something
+      return null;
+  }
 }
--- klid/src/C9045_REQMAPPINGMETHOD/REQMAPPINGMETHOD_GOOD.java
+++ klid/src/C9045_REQMAPPINGMETHOD/REQMAPPINGMETHOD_GOOD.java
@@ -3,12 +3,43 @@ package C9045_REQMAPPINGMETHOD;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+@Controller
 public class REQMAPPINGMETHOD_GOOD {
 
   @RequestMapping(value = "/greet", method = GET)
   public String greet(String greetee) {
-    // do something
-    return null;
+      // do something
+      return null;
+  }
+
+  @GetMapping("/greet")
+  public String greet2(String greetee) {
+      // do something
+      return null;
+  }
+
+  @PutMapping("/greet")
+  public String greet3(String greetee) {
+      // do something
+      return null;
+  }
+
+  @DeleteMapping("/greet")
+  public String greet4(String greetee) {
+      // do something
+      return null;
+  }
+
+  @PatchMapping("/greet")
+  public String greet5(String greetee) {
+      // do something
+      return null;
+  }
+
+  @PostMapping("/greet")
+  public String greet6(String greetee) {
+      // do something
+      return null;
   }
   
 }
