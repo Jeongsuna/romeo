2c8c5b5bd34415853c424fdb418932081406fee9
WonKoyung <wonkyoung@codemind.co.kr>
2022-05-18 11:18:49 +0900

    [KLID] 9023 good 코드 수정

--- klid/src/C9023_NOTIGNOINTERRUPT/NOTIGNOINTERRUPT_GOOD.java
+++ klid/src/C9023_NOTIGNOINTERRUPT/NOTIGNOINTERRUPT_GOOD.java
@@ -13,8 +13,10 @@ public class NOTIGNOINTERRUPT_GOOD extends Thread {
             while (true) {
                 Thread.sleep(1000);
             }
-        }catch (InterruptedException e) { // Noncompliant; logging is not enough
+        }catch (InterruptedException e) {
             LOGGER.log(Level.WARNING, "Interrupted!", e);
+            // Restore interrupted state...
+            Thread.currentThread().interrupt();
         }
     }
 
