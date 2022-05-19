11a9f9ddb75cbb2e91aec5017316acf749d206bc
Kim KyengJun <kkj@codemind.co.kr>
2022-05-19 10:21:05 +0900

    [KILD] romeo 소스코드 수정
    902D 모든 Bad case(32개) 추가

--- klid/src/C902D_LOOPTURE/LOOPTURE_Bad.java
+++ klid/src/C902D_LOOPTURE/LOOPTURE_Bad.java
@@ -2,8 +2,140 @@ package C902D_LOOPTURE;
 
 public class LOOPTURE_Bad {
     public static void main(String[] args) {
-        for (int i = 10; i < 10; i++) {  // compliant
-            // ...
+//region a>=0 , b>=0
+        for(int i = 10; i < 10; i++) {  // Nocompliant
+
         }
+
+        for(int i = 10; i > 10; i++) {  // Nocompliant
+
+        }
+
+        for(int i = 10; 10 < i; i++) {  // Nocompliant
+
+        }
+
+        for(int i = 10; 10 > i; i++) {  // Nocompliant
+
+        }
+
+        for(int i = 11; i < 10; i++) {  // Nocompliant
+
+        }
+
+        for(int i = 11; i <= 10; i++) {  // Nocompliant
+
+        }
+
+        for(int i = 11; 10 > i; i++) {  // Nocompliant
+
+        }
+
+        for(int i = 11; 10 >= i; i++) {  // Nocompliant
+
+        }
+
+        for(int i = 9; i > 10; i++) {  // Nocompliant
+
+        }
+
+        for(int i = 9; i >= 10; i++) {  // Nocompliant
+
+        }
+
+        for(int i = 9; 10 < i; i++) {  // Nocompliant
+
+        }
+
+        for(int i = 9; 10 <= i; i++) {  // Nocompliant
+
+        }
+        //endregion
+
+        //region a>=0, b<0
+        for(int i = 10; i < -10; i++) {  // Nocompliant
+
+        }
+
+        for(int i = 10; i <= -10; i++) {  // Nocompliant
+
+        }
+
+        for(int i = 10; -10 > i; i++) {  // Nocompliant
+
+        }
+
+        for(int i = 10; -10 >= i; i++) {  // Nocompliant
+
+        }
+        //endregion
+
+        //region a<0, b>=0
+        for(int i = -10; i > 10; i++) {  // Nocompliant
+
+        }
+
+        for(int i = -10; i >= 10; i++) {  // Nocompliant
+
+        }
+
+        for(int i = -10; 10 < i; i++) {  // Nocompliant
+
+        }
+
+        for(int i = -10; 10 <= i; i++) {  // Nocompliant
+
+        }
+        //endregion
+
+        //region a<0,b<0
+        for(int i = -10; i < -10; i++) {  // Nocompliant
+
+        }
+
+        for(int i = -10; i > -10; i++) {  // Nocompliant
+
+        }
+
+        for(int i = -10; -10 < i; i++) {  // Nocompliant
+
+        }
+
+        for(int i = -10; -10 > i; i++) {  // Nocompliant
+
+        }
+
+        for(int i = -9; i < -10; i++) {  // Nocompliant
+
+        }
+
+        for(int i = -9; i <= -10; i++) {  // Nocompliant
+
+        }
+
+        for(int i = -9; -10 > i; i++) {  // Nocompliant
+
+        }
+
+        for(int i = -9; -10 >= i; i++) {  // Nocompliant
+
+        }
+
+        for(int i = -11; i > -10; i++) {  // Nocompliant
+
+        }
+
+        for(int i = -11; i >= -10; i++) {  // Nocompliant
+
+        }
+
+        for(int i = -11; -10 < i; i++) {  // Nocompliant
+
+        }
+
+        for(int i = -11; -10 <= i; i++) {  // Nocompliant
+
+        }
+        
     }
 }
