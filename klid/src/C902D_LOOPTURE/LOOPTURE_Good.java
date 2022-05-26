8aa4e0c98be81c23c8e6227f7d4c74c08298471b
kimtaeyoung <taeyoung@codemind.co.kr>
2022-05-26 17:50:54 +0900

    [KLID] romeo 테스트 코드 수정
     9001/9008/902D/9027

--- klid/src/C902D_LOOPTURE/LOOPTURE_Good.java
+++ klid/src/C902D_LOOPTURE/LOOPTURE_Good.java
@@ -2,8 +2,8 @@ package C902D_LOOPTURE;
 
 public class LOOPTURE_Good {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {  // Noncompliant
-            // ...
+        for (int i = 9; i < 10; i++) {
+            // compliant
         }
     }
 }
