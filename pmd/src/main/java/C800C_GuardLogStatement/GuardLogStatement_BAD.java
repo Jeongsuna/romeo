b0e2fb05edae691a1abb4c7ce26db51e582c3194
Kim KyengJun <kkj@codemind.co.kr>
2022-06-14 15:20:20 +0900

    [PMD] 빌드오류 수정
    800C 테스트 코드 빌드 오류 수정

--- pmd/src/main/java/C800C_GuardLogStatement/GuardLogStatement_BAD.java
+++ pmd/src/main/java/C800C_GuardLogStatement/GuardLogStatement_BAD.java
@@ -1,12 +1,12 @@
 package C800C_GuardLogStatement;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class GuardLogStatement_BAD {
     public class Test extends Throwable{
         private void bindParameters(Throwable param1) {
-            Logger log = Logger.getLogger(Test.class);
+            Logger log = LogManager.getLogger(Test.class);
             log.debug("log something {}", param1);
         }
     }
--- pmd/src/main/java/C800C_GuardLogStatement/GuardLogStatement_GOOD.java
+++ pmd/src/main/java/C800C_GuardLogStatement/GuardLogStatement_GOOD.java
@@ -1,11 +1,12 @@
 package C800C_GuardLogStatement;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class GuardLogStatement_GOOD {
     public class Test extends Throwable{
         private void bindParameters(Throwable param1) {
-            Logger log = Logger.getLogger(Test.class);
+            Logger log = LogManager.getLogger(Test.class);
             if (log.isDebugEnabled()) {
                 log.debug("log something" + param1 + "concat strings");
             }
