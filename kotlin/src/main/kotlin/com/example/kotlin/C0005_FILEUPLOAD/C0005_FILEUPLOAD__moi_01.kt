ba4be07f959f2b75a077a3f96f5adeb9faf01f93
kimtaeyoung <taeyoung@codemind.co.kr>
2023-02-02 19:36:31 +0900

    [Kotlin] 예제 수정

--- kotlin/src/main/kotlin/com/example/kotlin/C0005_FILEUPLOAD/C0005_FILEUPLOAD__moi_01.kt
+++ kotlin/src/main/kotlin/com/example/kotlin/C0005_FILEUPLOAD/C0005_FILEUPLOAD__moi_01.kt
@@ -11,8 +11,9 @@ import java.util.*
 import java.util.logging.Level
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import net.coobird.thumbnailator.Thumbnails
 
-class C0005_FILEUPLOAD__simple_01 {
+class C0005_FILEUPLOAD__moi_01 {
     private lateinit var path: String
     private lateinit var path_bak: String
     private var size = 0
@@ -52,57 +53,7 @@ class C0005_FILEUPLOAD__simple_01 {
         }
 
         /* FLAW : CWE-434 */
-        createThumnail("$savePath/$fileName", "$savePath/s_$fileName", 150)
-    }
-
-    @Throws(IOException::class)
-    fun good(request: HttpServletRequest, response: HttpServletResponse, savePath: String) {
-        val sizeLimit = 5 * 1024 * 1024
-        val multi = MultipartRequest(request, savePath, sizeLimit.toLong(), "euc-kr", DefaultFileRenamePolicy())
-        val stemail = request.getParameter("stemail")
-        val stpwd = request.getParameter("stpwd")
-        val stcontent = request.getParameter("stcontent")
-        val stre_step = request.getParameter("stre_step")
-        val stre_num = request.getParameter("stre_num")
-        val fileName = multi.getFilesystemName("filename")
-        lateinit var con: Connection
-        lateinit var pstmt: PreparedStatement
-        if (fileName != null) {
-            val fileExt = fileName.substring(fileName.lastIndexOf(".") + 1).lowercase(Locale.getDefault())
-            if ("gif" != fileExt && "jpg" != fileExt && "png" != fileExt) {
-                alertMessage(response, "업로드 불가능한 파일입니다.")
-                return
-            }
-        }
-        try {
-            con = IO.getDBConnection()
-            val sql = (" INSERT INTO board(email,r_num,w_date,pwd,content,re_step,re_num,file-name) "
-                    + " values ( ?, 0, sysdate(), ?, ?, ?, ?, ? ) ")
-            pstmt = con.prepareStatement(sql)
-            pstmt.setString(1, stemail)
-            pstmt.setString(2, stpwd)
-            pstmt.setString(3, stcontent)
-            pstmt.setString(4, stre_step)
-            pstmt.setString(5, stre_num)
-            pstmt.setString(6, fileName)
-            pstmt.executeUpdate()
-        } catch (e: SQLException) {
-            IO.logger.log(Level.WARNING, e.toString())
-        } finally {
-            try {
-                pstmt.close()
-                con.close()
-            } catch (e: SQLException) {
-                IO.logger.log(Level.WARNING, e.toString())
-            }
-        }
-        createThumnail("$savePath/$fileName", "$savePath/s_$fileName", 150)
-    }
-
-    fun createThumnail(path: String, path_bak: String, size: Int) {
-        this.path = path
-        this.path_bak = path_bak
-        this.size = size
+        Thumbnails.of("'$savePath'/'$fileName'").size(150,150).toFile("'$savePath'/s_'$fileName'")
     }
 
     @Throws(IOException::class)
