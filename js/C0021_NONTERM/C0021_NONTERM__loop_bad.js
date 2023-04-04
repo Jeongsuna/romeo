6496e79f1fae5081d2c908002ac3d3fb5659be15
Kim KyengJun <juniair.kim@gmail.com>
2023-04-04 11:07:12 +0900

    [Samsung] 삼성화재 테스트 코드 작성

--- js/C0021_NONTERM/C0021_NONTERM__loop_bad.js
+++ js/C0021_NONTERM/C0021_NONTERM__loop_bad.js
@@ -1,10 +1,26 @@
 var data = "";
 
-while(true) {
+while(true) { // @violation
     data += "….";
 }
 
 // for 무한루프
-for (;;) {
+for (;;) { // @violation
     data += "….";
+}
+
+function f() {
+    for (;;) { // @violation
+        data += "….";
+        switch (condtion) {
+            case 1:
+            case 2:
+                break;
+            case 3:
+                break
+            default:
+                break
+
+        }
+    }
 }
