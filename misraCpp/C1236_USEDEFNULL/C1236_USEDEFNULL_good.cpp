b771f96aaa7ec260fb618e35b5946000c8e38efa
KimKyengJun <kkj@codemind.co.kr>
2023-11-14 15:46:18 +0900

    [CXX] MISRA C++ 2008 예제 파일명 변경

--- misraCpp/C1236_USEDEFNULL/C1236_USEDEFNULL_good.cpp
+++ misraCpp/C1236_USEDEFNULL/C1236_USEDEFNULL_good.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 #include <cstddef>
 
-void f1(int32_t val) {
+void f1(int val)
+{
     std::cout << "f1: " << val << std::endl;
 }
 
-void f2(int32_t* val) {
-    if (val == NULL) {
+void f2(int *val)
+{
+    if (val == NULL)
+    {
         std::cout << "f2: NULL" << std::endl;
-    } else {
+    }
+    else
+    {
         std::cout << "f2: " << *val << std::endl;
     }
 }
 
-void f3() {
-    f1(NULL); // Not-compliant, NULL used as an integer
+void f3()
+{
+    f1(0);    // Use 0 instead of NULL
     f2(NULL); // Compliant
 }
 
-int main() {
-    int32_t val = 10;
+int main()
+{
+    int val = 10;
     f1(val);
     f2(&val);
     f3();
