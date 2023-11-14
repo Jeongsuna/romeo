b2a98af098bc85cd04ae722fc63b2beaa45be06e
KimKyengJun <kkj@codemind.co.kr>
2023-11-14 15:23:14 +0900

    [CXX] MISRA C++ 1237 체커 관련 파일 수정
    현재 int32_t(signed int)를 분석이 안되어 int 타입으로 변경

--- misraCpp/LITNULLPTR_1237/LITNULLPTR_1237__bad.cpp
+++ misraCpp/LITNULLPTR_1237/LITNULLPTR_1237__bad.cpp
@@ -2,25 +2,32 @@
 #include <cstddef>
 
 // 함수 정의
-void f1(int32_t val) {
+void f1(int val)
+{
     std::cout << "f1: " << val << std::endl;
 }
 
-void f2(int32_t* val) {
-    if (val == NULL) {
+void f2(int *val)
+{
+    if (val == NULL)
+    {
         std::cout << "f2: NULL" << std::endl;
-    } else {
+    }
+    else
+    {
         std::cout << "f2: " << *val << std::endl;
     }
 }
 
-void f3() {
+void f3()
+{
     f1(0); // Compliant
     f2(0); // Non-compliant, 0 used as the null pointer constant
 }
 
-int main() {
-    int32_t val = 10;
+int main()
+{
+    int val = 10;
     f1(val);
     f2(&val);
     f3();
