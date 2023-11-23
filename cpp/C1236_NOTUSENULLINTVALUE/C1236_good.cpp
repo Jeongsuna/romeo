1338317b2b679800d86480b62869f9ec32a64b85
KimKyengJun <kkj@codemind.co.kr>
2023-11-23 18:35:22 +0900

    [Misra] Misra C++ 예제 수정
    - C1236 좋은 예제 데이터 타입 수정

--- cpp/C1236_NOTUSENULLINTVALUE/C1236_good.cpp
+++ cpp/C1236_NOTUSENULLINTVALUE/C1236_good.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <stdint.h>
+#include <stdint32_t.h>
 
-void func(int a) 
+void func(int32_t a) 
 {
   std::cout << a << std::endl;
 }
 
-int func2() {
+int32_t func2() {
     return 0;  // 수정됨 : 함수 반환에서 NULL 사용
 }
 
@@ -15,18 +15,18 @@ int main()
   
   func(0);  // 수정됨 : 함수 호출에서 NULL 사용
   
-  int a = 0 + 1;  // 수정됨 : 산술 연산에서 NULL 사용
+  int32_t a = 0 + 1;  // 수정됨 : 산술 연산에서 NULL 사용
 
   if (5 == 0) {}  // 수정됨 : 비교 연산에서 NULL 사용
   
-  int b = 0;  // 수정됨 : 할당에서 NULL 사용
+  int32_t b = 0;  // 수정됨 : 할당에서 NULL 사용
 
-  int arr[10];
-  int i = arr[0];  // 수정됨 : 배열 인덱스에서 NULL 사용
+  int32_t arr[10];
+  int32_t i = arr[0];  // 수정됨 : 배열 인덱스에서 NULL 사용
 
-  int c = 5 & 0;  // 수정됨 : 비트 연산에서 NULL 사용
+  int32_t c = 5 & 0;  // 수정됨 : 비트 연산에서 NULL 사용
 
-  int d = (true ? 0 : 1);  // 수정됨 : 조건 연산자에서 NULL 사용
+  int32_t d = (true ? 0 : 1);  // 수정됨 : 조건 연산자에서 NULL 사용
 
   return 0;
 }
