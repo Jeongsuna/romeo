0b6865f1fa670497dcf42fbf4673a0cf67636f52
ganghyeon <ganghyeon@codemind.co.kr>
2023-11-29 17:00:52 +0900

    [Misra] Misra C++ 예제 추가(126B ~ 1295)
    - 126B, 126C, 126E, 126F
    - 127C, 127D, 127F, 1271, 1295

---
+++ cpp/C126B_USESWITCHDEFAULT/C126B_bad.cpp
@@ -0,0 +1,25 @@
+int main(){
+    int int16 = 1;
+
+    //  switch 문을 default 절 없이 사용하고 있습니다.(비준수)
+    switch (int16)
+    {
+        case 0:
+            break;
+        case 1:
+        case 2:
+            break;
+    }
+
+    //  switch 문이 (enum값)RED와 GREEN 두 가지 경우만을 처리 하고 있습니다.(비준수)
+    enum Colours { RED, BLUE, GREEN } colour;
+    switch (colour)
+    {
+        case RED:
+            break;
+        case GREEN:
+            break;
+    }
+
+    return 0;
+}
---
+++ cpp/C126B_USESWITCHDEFAULT/C126B_good.cpp
@@ -0,0 +1,37 @@
+int main(){
+    int int16 = 1;
+    // default 절을 제공하여 나머지 경우를 처리합니다.(준수)
+    switch (int16)
+    {
+        case 0:
+            break;
+        case 1:
+        case 2:
+            break;
+        default:
+            break;
+    }
+
+    // switch 문에서 enum 값을 처리하는 경우
+    enum Colours { RED, BLUE, GREEN } colour;
+    switch (colour)
+    {
+        // 방법 1.  enum 값의 모든 가능한 경우를 처리 해야합니다.(준수)
+        case RED:
+            break;
+        case BLUE:
+            break;
+        case GREEN:
+            break;
+
+        // 방법 2. default 절을 제공하여 나머지 경우를 처리합니다.(준수)
+        // case RED:
+        //     break;
+        // case GREEN:
+        //     break;
+        // default:
+        //     break;
+    }
+
+    return 0;
+}
