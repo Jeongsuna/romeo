829de8ed1df862434c0f4037f2a520364b6bb4a7
Jemin Kim <jemin@codemind.co.kr>
2023-12-07 16:46:20 +0900

    [Misra] Misra C++ 2008 예제 추가(1287)

---
+++ cpp/C1287_NOTRETPARREFPASSEDBY/C1287__simple_bad.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int32_t * fn1(int32_t &x)
+{
+    //int32_t 형식의 참조 매개변수(x)를 받고, 포인터로 반환하려고 합니다.(비준수)
+    return (&x); 
+}
+
+int32_t * fn2()
+{
+    int32_t i = 0;
+    return fn1(i);
+}
+
+const int32_t * fn3(const int32_t &x)
+{
+    //const int32_t 형식의 상수 참조 매개변수(x)를 받고, 포인터로 반환하려고 합니다.(비준수)
+    return (&x); 
+}
+
+int32_t & fn4(int32_t &x)
+{
+    // fn4 함수는 int32_t 형식의 참조 매개변수(x)를 받고, 참조로 반환하려고 합니다.(비준수)
+    return (x); 
+}
+
+const int32_t & fn5(const int32_t &x)
+{
+    // fn5 함수는 const int32_t 형식의 상수 참조 매개변수(x)를 받고, 참조로 반환하려고 합니다.(비준수)
+    return (x); 
+}
+
+int main(void){return 0;}
