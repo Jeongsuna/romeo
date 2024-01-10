de05e4c0c532b02a0154aaa22d91b9fc895a41a9
yujin <yujin@codemind.co.kr>
2024-01-10 09:25:51 +0900

    [Misra] Misra C 예제 수정
    - 10D3 good/bad 파일명 변경
    - 10DC 폴더 및 파일명에 불필요한 띄어쓰기 제거

--- misra/10D3_MESSTYPE5/10D3_MESSTYPE5__BadCode.cpp
+++ misra/10D3_MESSTYPE5/10D3_MESSTYPE5__BadCode.cpp
@@ -1,16 +1,10 @@
 #include <cstdint>
 #define bool_t bool
 
-int fake_main()
+int main()
 {
-	enum enuma { A1, A2, A3 } ena;
-	enum enumc { C1, C2, C3 } enc;
-
-	(bool_t)false;    /* Compliant - C99 'false' is essentially Boolean */
-	(int32_t)3U;      /* Compliant */
-	(bool_t)0;       /* Compliant - by exception */
-	(int32_t)ena;     /* Compliant */
-	(char)enc;        /* Compliant */
+	(bool_t)3U;           /* Non-compliant */
+	(enum enuma)3;        /* Non-compliant */
 
 	return 0;
 }
