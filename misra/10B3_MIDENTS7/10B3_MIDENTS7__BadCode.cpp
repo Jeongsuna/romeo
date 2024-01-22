fc51d14eeba063d2f447980b1e20d11f40a48da8
ganghyeon <ganghyeon@codemind.co.kr>
2024-01-22 09:00:39 +0900

    [Misra] Misra C 예제 수정(10B3)
    [Misra c 2012]
    - 10B3 예제 수정

--- misra/10B3_MIDENTS7/10B3_MIDENTS7__BadCode.cpp
+++ misra/10B3_MIDENTS7/10B3_MIDENTS7__BadCode.cpp
@@ -1,10 +1,4 @@
-// #include <arm64_neon.h> 추가
-// main() 추가
-// 솔루션을 ARM64로 설정 후 빌드
-// struct elk 둘 중 하나 주석처리
-// union과 stag 호환 안되므로 주석처리
-
-#include <arm64_neon.h>
+#include <cstdint>
 
 struct stag
 {
@@ -12,7 +6,7 @@ struct stag
 	uint16_t b;
 };
 struct stag a1 = { 0, 0 }; /* Compliant - compatible with above */
-//union stag a2 = { 0, 0 }; /* Non-compliant - declares different type
+union stag a2 = { 0, 0 }; /* Non-compliant - declares different type
 //* from struct stag.
 //* Constraint violation in C99 */
 struct deer
@@ -36,11 +30,11 @@ struct elk
 {
 	uint16_t x;
 };
-//struct elk /* Non-compliant - declaration of different type
-//* Constraint violation in C99 */
-//{
-//	uint32_t x;
-//};
+struct elk /* Non-compliant - declaration of different type
+* Constraint violation in C99 */
+{
+	uint32_t x;
+};
 
 int main()
 {
