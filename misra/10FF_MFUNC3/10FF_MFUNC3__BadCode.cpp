f797941bf4a5202b8d79f64f1306f4b4ea6379a9
bumseok <bumseok@codemind.co.kr>
2024-01-22 15:17:59 +0900

    [Misra] Misra C++ 예제 수정
    - 112D, 112F : 수정

--- misra/10FF_MFUNC3/10FF_MFUNC3__BadCode.cpp
+++ misra/10FF_MFUNC3/10FF_MFUNC3__BadCode.cpp
@@ -1,12 +1,12 @@
 extern double power_10ff_1(double d, int n) { return 1; };
 
-void func2(void)
+void func1(void)
 {
 	/* Non-compliant - return type and both argument types incorrect */
 	double sql = power_10ff_1(1, 2.0);
 }
 
-int fake_main()
+int main()
 {
 	return 0;
 }
--- misra/10FF_MFUNC3/10FF_MFUNC3__GoodCode.cpp
+++ misra/10FF_MFUNC3/10FF_MFUNC3__GoodCode.cpp
@@ -1,12 +1,12 @@
-extern double power_10ff_2(double d, int n);
+extern double power_10ff_2(double d, int n) { return 1; };
 
 void func2(void)
 {
     /* Now compliant - function is declared before use */
-    double sql = power_10ff_2(1.0, 2);
+    double sql = power_10ff_2((double)1, (int)2.0);
 }
 
-int fake_main()
+int main()
 {
     return 0;
 }
