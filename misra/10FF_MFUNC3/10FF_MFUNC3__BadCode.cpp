825172e4f40c2b49d4a2543c2f83480e13e916d7
bumseok <bumseok@codemind.co.kr>
2024-01-25 15:21:00 +0900

    [Misra] Misra C++ 예제 수정
    - 10FF : 수정

--- misra/10FF_MFUNC3/10FF_MFUNC3__BadCode.cpp
+++ misra/10FF_MFUNC3/10FF_MFUNC3__BadCode.cpp
@@ -1,12 +1,11 @@
-extern double power_10ff_1(double d, int n) { return 1; };
+#include <stdio.h>
 
-void func1(void)
-{
-	/* Non-compliant - return type and both argument types incorrect */
-	double sql = power_10ff_1(1, 2.0);
+int main(void) {
+    int result = addNumbers1(5, 3); // addNumbers 함수가 선언되지 않았으므로 암묵적 선언 발생
+    printf("Result is: %d\n", result);
+    return 0;
 }
 
-int main()
-{
-	return 0;
+int addNumbers1(int a, int b) {
+    return a + b;
 }
--- misra/10FF_MFUNC3/10FF_MFUNC3__GoodCode.cpp
+++ misra/10FF_MFUNC3/10FF_MFUNC3__GoodCode.cpp
@@ -1,12 +1,13 @@
-extern double power_10ff_2(double d, int n) { return 1; };
+#include <stdio.h>
 
-void func2(void)
-{
-    /* Now compliant - function is declared before use */
-    double sql = power_10ff_2((double)1, (int)2.0);
+int addNumbers2(int a, int b); // 함수 프로토타입 선언
+
+int main(void) {
+    int result = addNumbers2(5, 3);
+    printf("Result is: %d\n", result);
+    return 0;
 }
 
-int main()
-{
-    return 0;
+int addNumbers2(int a, int b) {
+    return a + b;
 }
