0f0823cc31dbc5c3e99f80301f078e54c924a6a6
yujin <yujin@codemind.co.kr>
2024-03-19 17:19:05 +0900

    [Java] 8035 pmd 예제 추가
    - util class(static 메서드만 있는 class)에 대한 예제 추가

--- pmd/src/main/java/C8035_ClassNamingConventions/ClassNamingConventions_GOOD.java
+++ pmd/src/main/java/C8035_ClassNamingConventions/ClassNamingConventions_GOOD.java
@@ -2,4 +2,12 @@ package C8035_ClassNamingConventions;
 
 public class ClassNamingConventions_GOOD {
     public class TestClass {}
+
+    public class ClassNamingConventionsUtilClass {
+        public static String utilData;
+
+        public static String getUtilData() {
+            return utilData;
+        }
+    }
 }
