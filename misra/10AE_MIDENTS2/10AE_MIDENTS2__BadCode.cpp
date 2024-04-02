65b8185ba00490921c04e84c383611c51d2efac9
taeyoung <taeyoung@codemind.co.kr>
2024-04-02 13:40:51 +0900

    Misra] Modify Misra example
    - modify : 10AE

--- misra/10AE_MIDENTS2/10AE_MIDENTS2__BadCode.cpp
+++ misra/10AE_MIDENTS2/10AE_MIDENTS2__BadCode.cpp
@@ -1,12 +1,23 @@
 #include <cstdint>
+//C90
 extern int engine_exhaust_gas_temperature_raw;
 static int engine_exhaust_gas_temperature_scaled; /* Non-compliant */
 void f(void)
 {
 	int engine_exhaust_gas_temperature_local; /* Compliant */
 }
+
 static int engine_exhaust_gas_temp_raw;
 static int engine_exhaust_gas_temp_scaled; /* Compliant */
+
+//C99
+extern int engine_exhaust_gas_temperature_engine_exhaust_gas_temperature_raw;
+static int engine_exhaust_gas_temperature_engine_exhaust_gas_temperature_scaled; /* Non-compliant */
+void f(void)
+{
+	int engine_exhaust_gas_temperature_engine_exhaust_gas_temperature_local;
+    int engine_exhaust_gas_temperature_engine_exhaust_gas_temperature_locale; /* Non-compliat */
+}
 int main() {
 	f();
 	return 0;
